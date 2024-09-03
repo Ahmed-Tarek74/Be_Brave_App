4f502c0399e4b4a15734fb5cca4496e57554b234
admin <ahmed.tarek200074@gmail.com>
2024-09-03 12:08:15 +0300

    enhancements

--- app/src/main/java/com/compose/chatapp/di/UseCaseModule.kt
+++ app/src/main/java/com/compose/chatapp/di/UseCaseModule.kt
@@ -20,60 +20,63 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
 
 @Module
 @InstallIn(ViewModelComponent::class)
 object UseCaseModule {
+
+    @ViewModelScoped
     @Provides
     fun provideLoginUseCase(
         authRepository: AuthRepository,
         deviceTokenRepository: DeviceTokenRepository,
         userRepository: UserRepository
     ): LoginUseCase = LoginUseCase(authRepository, deviceTokenRepository, userRepository)
-
+    @ViewModelScoped
     @Provides
     fun provideRegistrationUseCase(authRepository: AuthRepository, userRepository: UserRepository):
             RegistrationUseCase = RegistrationUseCase(authRepository, userRepository)
-
+    @ViewModelScoped
     @Provides
     fun provideDateFormatterUseCase():
             DateFormatterUseCase = DateFormatterUseCase()
-
+    @ViewModelScoped
     @Provides
     fun provideGetUsersUseCase(userRepository: UserRepository):
             GetUsersUseCase = GetUsersUseCase(userRepository)
-
+    @ViewModelScoped
     @Provides
     fun provideLogOutUseCase(
         authRepository: AuthRepository,
         userRepository: UserRepository
     ):
             LogOutUseCase = LogOutUseCase(authRepository, userRepository)
-
+    @ViewModelScoped
     @Provides
     fun provideGetCachedUserUseCase(userRepository: UserRepository):
             GetCachedUserUseCase = GetCachedUserUseCase(userRepository)
-
+    @ViewModelScoped
     @Provides
     fun provideSendMessageUseCase(
         messagesRepository: MessagesRepository,
         recentChatsRepository: RecentChatsRepository,
     ): SendMessageUseCase =
         SendMessageUseCase(messagesRepository, recentChatsRepository)
-
+    @ViewModelScoped
     @Provides
     fun provideSendNotificationUseCase(
         notificationRepository: NotificationRepository,
         deviceTokenRepository: DeviceTokenRepository
     ): SendNotificationUseCase =
         SendNotificationUseCase(notificationRepository, deviceTokenRepository)
-
+    @ViewModelScoped
     @Provides
     fun provideGetRecentChatsUseCase(
         recentChatsRepository: RecentChatsRepository,
     ):
             GetRecentChatsUseCase = GetRecentChatsUseCase(recentChatsRepository)
-
+    @ViewModelScoped
     @Provides
     fun provideGetRecentMessagesUseCase(messagesRepository: MessagesRepository):
             GetRecentMessagesUseCase = GetRecentMessagesUseCase(messagesRepository)
